bf10b334bf75a2eb0c0e60dba5463d3bb22dad85
mvan231 <50910610+mvan231@users.noreply.github.com>
2020-11-10 10:59:53 -0500

    Update to v1.1

--- RH-Downloads.js
+++ RH-Downloads.js
@@ -1,39 +1,23 @@
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: green; icon-glyph: grin-tongue-squint;
 /*
 
 RH-Downloads by mvan231
 
-v1.0
+v1.1 Add support for checking multiple links and cycle through them with each refresh of the widget 
+
+v1.0 Initial Release
 
 */
 
 /*
 #####
-   Initialization and HTML Grab - End
+   Initialization and HTML Grab
 #####
 */
+
+//to check different accounts / shortcuts on each run, add another URL to the url array below. They must be separated by a comma ',' and must be enclosed in single quotes.                                                                                                                                
 let wv=new WebView()
-const url = 'https://routinehub.co/user/mvan231'
-await wv.loadURL(url)
+var url = ['https://routinehub.co/shortcut/5583/','https://routinehub.co/user/mvan231','https://routinehub.co/shortcut/5195']
 
-let html = await wv.getHTML()
-
-var reg = /\<p\>Downloads\: (\d+)\<\/p>/
-var str
-var lastUpd
-
-if (reg.exec(html))
-  {
-  str = html.match(reg)[1]
-  console.log(str)
-  }
-/*
-#####
-   Initialization and HTML Grab - End
-#####
-*/
 
 /*
 #####
@@ -44,34 +28,35 @@ const now = new Date()
 let ff = new DateFormatter()
 ff.dateFormat = 'd-MMM H:mm'
 let frmt = ff.string(now)
-console.log(frmt)
-// log(now.getDate())
+log(frmt)
 
 ab = FileManager.iCloud()
 dir=ab.documentsDirectory()
 let path = dir+"/RhLastUpd.json"
 log(path)
 let file
-
+//let temp ={}
 if (!ab.fileExists(path))
 {
   //if file does not exist
   file={}
-  file.downloads = str
   file.date = now.getDate()
+  file.index = -1
   ab.write(path, Data.fromString(JSON.stringify(file)))
   ab.writeString(path, JSON.stringify(file))
 }
   //parse the existing file or the newly created file
   file = JSON.parse(ab.readString(path))
-  log(file.downloads)
   log(file.date)
+  log("index is "+file.index)
 
-// if file doesnt contain dayDelta key:value
-if (!file.dayDelta)
-{
-  file.dayDelta = 0
-}
+// if file doesn't contain the index key:value
+if (!file.index && (!String(file.index).length))file.index = -1 
+//if (file.hasOwnProperty('index'))file.index = -1
+
+/*if (!file.weekDelta)
+*/
+
 /*
 #####
    End iCloud Storage File Information
@@ -80,31 +65,108 @@ if (!file.dayDelta)
 
 /*
 #####
+   Grab HTML and Parse
+#####
+*/
+log("index is "+file.index)
+
+log(url.length)
+file.index += 1
+log("index is now "+file.index)
+if (file.index >= url.length)
+{
+ file.index = 0 
+}
+log(file.index)
+url = url[file.index]
+log(url)
+//initialize the name variable
+var name
+//initialize the regex variable
+var reg
+
+
+await wv.loadURL(url)
+
+let html = await wv.getHTML()
+
+// check if the current (to use) URL contains "user" or not
+if (url.includes("user"))
+{
+  reg = /user\/(.+)(\/|)/
+  if (reg.exec(url))
+  {
+    name = url.match(reg)[1]
+    log("match is "+url.match(reg)[1])
+  }  
+}else {
+  reg = /•\s(.+)\<\/title\>/
+  if (reg.exec(html))
+  {
+    name = html.match(reg)[1]
+    log("match is "+html.match(reg)[1])
+  }
+}
+log(name)
+
+reg = /\<p\>Downloads\: (\d+)\<\/p>/
+var str
+var lastUpd
+
+if (reg.exec(html))
+  {
+  str = html.match(reg)[1]
+  log(str)
+  }else{
+    window.stop();
+  }
+
+log(file[name])
+
+if (!file[name])
+{
+file[name] = {
+  downloads:str,
+  dayDelta:0
+  }
+}else{
+  //temp.dayDelta=file[name].dayDelta
+  //temp.downloads=file[name].downloads
+}
+/*
+#####
    start download diff calculation
 #####
 */
 var diff
 
-if (file.date == now.getDate())
+if (file.date != now.getDate())
   {
-   //   if the current date is the same as the date last saved in file 
-  }else if (file.date == (now.getDate() - 1))
-    {
-      // if the date saved in file is yesterday (only should happen on first run in a new day to reset the delta download count)
-      file.dayDelta = 0
-    }
+    file[name].dayDelta = 0
+  }
     
   // calculate difference between new RH count, file download count and the previous delta value. this shows the change in downloads throughout the current day
-  diff = (str - file.downloads) + file.dayDelta
+  diff = (str - file[name].downloads) + file[name].dayDelta
 
-  //   assign dictionary items new Values
-  file.dayDelta = diff
-  file.downloads = str
-  file.date = now.getDate()
+/*
+#####
+   finish diff calculation
+#####
+*/
 
-// save new Values to file
-  //   ab.write(path, Data.fromString(JSON.stringify(file)))
-  ab.writeString(path, JSON.stringify(file))
+//   assign dictionary items new Values
+/*temp.dayDelta = diff
+temp.downloads = str*/
+
+file.date = now.getDate()
+//file[name] = temp
+file[name] = {
+  dayDelta:diff,
+  downloads:str
+}
+
+// save file with updates
+ab.writeString(path, JSON.stringify(file))
 
 
 /*
@@ -118,6 +180,13 @@ if (file.date == now.getDate())
   const main = w.addText("RH Downloads")
   main.textColor = Color.gray()
   main.centerAlignText()
+
+  if (name)
+  {
+    const nameText = w.addText(name)
+    nameText.textColor=Color.gray()
+    nameText.centerAlignText()
+  }
 
   // add current Date to widget
   const sub = w.addText(frmt)
@@ -141,9 +210,6 @@ if (file.date == now.getDate())
     }
   
   w.addSpacer(6)
-  
-
-  
   //set the widget
   Script.setWidget(w)
   //complete the script
@@ -154,13 +220,3 @@ if (file.date == now.getDate())
    end widget
 #####
 */
-
-/*
-#####
-  start functions
-#####
-*/
-
-function log(inp){
- console.log(inp) 
-}
