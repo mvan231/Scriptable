9975f00f22b32b40780d0c7462e5f36f30485b54
mvan231 <50910610+mvan231@users.noreply.github.com>
2020-12-02 17:55:00 -0500

    v1.5

--- MEE6 LeaderBoard Info.js
+++ MEE6 LeaderBoard Info.js
@@ -4,29 +4,37 @@
 /*---------------------
 set the justification you want: left,right,center
 ---------------------*/
+
 let justify = "left"
+
 /*---------------------
 add the guildIds you want to cycle through on each refresh of the widget
 ---------------------*/
+
 let url =['491379054331559936','760937324711641108']
 
 /*---------------------
 image background, set to true to use an image as the background of the widget
 ---------------------*/
+
 const imgBack = false
 let img
+
 /*---------------------
 font color setup
 
 below colors are in this order for light/dark mode (lightColor, darkColor)
 ---------------------*/
-
+//title and progress bar color
 let titleCol = Color.dynamic(Color.blue(),Color.red())
+
+//body color
 let bodyCol = Color.dynamic(Color.darkGray(), Color.lightGray())
 
 /*---------------------
 start setup
 ---------------------*/
+
 let fm = FileManager.iCloud()
 log(config.runsInWidget)
 let b, urlBase = 'https://mee6.xyz/api/plugins/levels/leaderboard/'
@@ -58,7 +66,6 @@ if (imgBack && fm.fileExists(dir+"/MEE6back.jpg"))
 {
   img = fm.readImage(dir+"/MEE6back.jpg")
 }
-  
 b = fm.readString(path)
 log(b)
 b=JSON.parse(b)
@@ -76,13 +83,12 @@ log(url)
 /*---------------------
 
 ---------------------*/
-
 /*
 #####
 Update Check
 #####
 */
-let version = "1.4"
+let version = "1.5"
 
 let updateCheck = new Request('https://raw.githubusercontent.com/mvan231/Scriptable/main/MEE6.json')
 let uC = await updateCheck.loadJSON()
@@ -95,7 +101,6 @@ if (uC.version != version){
   if (!config.runsInWidget)
   {
   log("running standalone")
-
   let upd = new Alert()
   upd.title="Server Version Available"
   upd.addAction("OK")
@@ -116,10 +121,10 @@ End Update Check
 #####g
 */
 
-
 /*---------------------
 get data from current url item
 ---------------------*/
+
 let wv = new WebView()
 await wv.loadURL(url)
 wv.waitForLoad()
@@ -128,25 +133,61 @@ let html = await wv.getHTML()
 /*---------------------
 parse the results from the leaderboard to find your username
 ---------------------*/
+
 var regex = new RegExp('.*'+b.uName+'.*')
 let r = new Request(url)
 json = await r.loadJSON()
 let str = json.players
 log(json)
 log(json.guild.name)
-let rank,xp,level,count
+let rank,xp,level,count,id,avatar,progress,nextLevel
 str.forEach(f)
 log(rank)
 
 /*---------------------
 setup the widget
 ---------------------*/
+// progress
+if (config.widgetFamily == "medium")
+{
+var wid = 325
+}else{
+var wid = 140
+}
+const width = wid
+const h=5
+
+
+
+
+
 let w = new ListWidget()
-w.setPadding(2, 8, 2, 8)
+w.setPadding(10,10,10, 10)
+
+let re = new Request("https://cdn.discordapp.com/avatars/"+userId+"/"+avatar+".png?size=1024")
+
+log("https://cdn.discordapp.com/avatars/"+userId+"/"+avatar+".png?size=1024")
+//"https://cdn.discordapp.com/avatars/360097325730889730/5d5ce4e5f22c50495f200801c61c101a.png?size=1024"
+log(dir+'/'+avatar+'.png')
+let pimg
+if(!fm.fileExists(dir+'/'+avatar+'.png')){
+ log("file doesn't exist, downloading...")
+    pimg = await re.loadImage()
+    fm.writeImage(dir+'/'+avatar+'.png', pimg)
+}else{
+log("file exists, loading now from iCloud Drive...")
+}
+pimg = fm.readImage(dir+'/'+avatar+'.png')
+
+let addIm = w.addImage(pimg)
+addIm.cornerRadius = 30
+addIm.centerAlignImage()
 rank=rank.toString()
 xp=xp.toString()
 level=level.toString()
 count =count.toString()
+progress=progress.toString()
+nextLevel=nextLevel.toString()
 var upText = needUpdate?"\nUpdate Available":""
 let title = w.addText(json.guild.name + upText)
 title.textColor=titleCol //Color.blue()
@@ -158,15 +199,19 @@ fonter("Rank: "+rank, 13)
 fonter("Level: "+level,13)
 fonter("XP: "+xp,13)
 fonter("MsgCount: "+count,13)
+//detailed XP item in JSON explained
+//Current progress [0] next level [1] and total xp [2]
+const imgw = w.addImage(Progress(nextLevel,progress))
+imgw.imageSize=new Size(width, h)
 if(imgBack)w.backgroundImage=img
 Script.setWidget(w)
 Script.complete()
 w.presentSmall()
 
-
 /*---------------------
 start function section
 ---------------------*/
+
 function fonter(text,size){
   let a = w.addText(text)
   a.font=Font.lightRoundedSystemFont(size)
@@ -186,5 +231,27 @@ async function f(inp,index){
     xp = cc.xp
     level = cc.level
     count=cc.message_count
+    avatar=cc.avatar
+    userId = cc.id
+    progress = cc.detailed_xp[0]
+    nextLevel = cc.detailed_xp[1]
   }
+}
+
+function Progress(total,havegone){
+    const context =new DrawContext()
+    context.size=new Size(width, h)
+    context.opaque=false
+    context.respectScreenScale=true
+    context.setFillColor(Color.darkGray())
+    const path = new Path()
+    path.addRoundedRect(new Rect(0, 0, width, h), 3, 2)
+    context.addPath(path)
+    context.fillPath()
+    context.setFillColor(titleCol)
+    const patha= new Path()
+    patha.addRoundedRect(new Rect(0, 0, width*havegone/total, h), 3, 2)
+    context.addPath(patha)
+    context.fillPath()
+    return context.getImage()
 }
