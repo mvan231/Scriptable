eb7ecdaaff1a72ce4564f3c2b0cab89904790125
mvan231 <50910610+mvan231@users.noreply.github.com>
2021-01-25 04:45:25 -0500

    Add files via upload

--- ScriptBackup.js
+++ ScriptBackup.js
@@ -1,6 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: deep-green; icon-glyph: magic;
+// icon-color: blue; icon-glyph: sync-alt;
 /*
 ##########
 
