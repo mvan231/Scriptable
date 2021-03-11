081630c98d7339d66135c51abf1568b4d360131f
mvan231 <50910610+mvan231@users.noreply.github.com>
2021-03-11 06:18:32 -0500

    V2
    Added support for reminders and following similar theme to Upcoming Calendar Indicator widget

--- Upcoming Calendar Widget.js
+++ Upcoming Calendar Widget.js
@@ -12,6 +12,8 @@ begin configuration
 
 const eventsToShow = 10
 
+//sets whether or not to display reminders in the widget
+const showReminders = false
 /*
 ####################
 ####################
@@ -39,6 +41,8 @@ main.layoutHorizontally()
 left.layoutVertically()
 
 let dF = new DateFormatter()
+dF.dateFormat='ZZ'
+let tZOffsetSec = (dF.string(new Date())/100)*60*60
 await CalendarEvent.thisWeek().then(successCallback, failureCallback)
 Script.setWidget(w)
 Script.complete()
@@ -59,7 +63,24 @@ async function successCallback(result) {
   await CalendarEvent.nextWeek().then((res) => {
     newCalArray = res
   })
-  newCalArray = mergeArrays(calcal,newCalArray)
+  if(showReminders){
+    remin = await Reminder.allDueThisWeek()
+    reminNext=await Reminder.allDueNextWeek()
+    newCalArray = mergeArrays(calcal,newCalArray,remin,reminNext)
+  }else{
+    newCalArray = mergeArrays(calcal,newCalArray)
+  }
+  newCalArray=JSON.stringify(newCalArray).replace(/dueDate/g, 'startDate')
+  
+  newCalArray=JSON.parse(newCalArray)
+// Sort array by date in ASCENDING order
+  newCalArray.sort(function (a, b) {
+    if (a.startDate > b.startDate) return 1;
+    if (a.startDate < b.startDate) return -1;
+    return 0;
+  });
+  //newCalArray.forEach(eventCount)
+
   newCalArray.forEach(f)
 }
 
@@ -89,8 +110,26 @@ function getColorDot(colorHex){
 }
 
 function f(item){
+  const date = new Date(); 
+  let isCalEvent
+  if('endDate' in item){
+    isCalEvent=true
+  }else{
+    isCalEvent=false
+  }  
   let now = new Date()
-  log(item.title)
+  dF.dateFormat='yyyy-MM-dd HH:mm:ss.SSSZ'
+  let dateString = item.startDate.toString()
+  dateString=dateString.replace('T', ' ')
+//   log(dateString)
+  item.startDate = dF.date(dateString)
+  if(isCalEvent){
+    dateString=item.endDate.toString()
+    dateString=dateString.replace('T',' ')
+    item.endDate = dF.date(dateString)
+  }
+//   log(item.startDate+'\n'+isCalEvent?item.endDate:'')
+//   log(item)  log(item.title)
   log('now '+now.getTime())  
   log('startTime '+item.startDate.getTime())
 
@@ -114,45 +153,45 @@ log(h)
         
       function addIt(){
           
-        let dot = getColorDot(item.calendar.color.hex)
+        //let dot = getColorDot(item.calendar.color.hex)
         s.setPadding(5,5,5,5)
         s.size= new Size(150, 27)
         let s1 = s.addStack()
         let imStack = s1.addStack()
         imStack.setPadding(2,0,2,0)
-        let im = imStack.addImage(dot)
+        //let im = imStack.addImage(dot)
+        //im.resizable=false
         imStack.layoutVertically()        
         let titleS = s1.addStack()
-        im.resizable=false
         let titleS1 = titleS.addStack()
-        let tx = titleS1.addText(item.title)
+        let tx = s1.addText((isCalEvent?'':item.isCompleted?'☑':'☐')+item.title)
+        //let tx = titleS1.addText(item.title)
+        tx.font=Font.systemFont(12)  
+        tx.textColor= new Color(item.calendar.color.hex)
         titleS1.layoutVertically()
-        tx.font=Font.systemFont(12)
         let s2=s.addStack()
         let dd = item.startDate
         dF.dateFormat='MMM d'
         let ddd=dF.string(dd)
         dF.dateFormat='EEE'
         let eee = dF.string(dd)
-        let dt = s2.addText(eee+' '+ddd+' ')
+        let dt = eee+' '+ddd+' '
+        //let dt = s2.addText(eee+' '+ddd+' ')
         dt.font=Font.systemFont(8)
-        if(!item.isAllDay){
-          let staTime = s2.addDate(item.startDate)
-          let sep = s2.addText('-')
-          sep.font=Font.systemFont(8)
-          let finTime = s2.addDate(item.endDate)
-          finTime.applyTimeStyle()
-          finTime.font=Font.systemFont(8)
-          staTime.applyTimeStyle()
-          staTime.font=Font.systemFont(8)
+        if(!item.isAllDay){    
+          dF.dateFormat= 'h:mma'
+          let sAndFTimes = isCalEvent?dF.string(item.startDate)+'-'+dF.string(item.endDate):dF.string(item.startDate)            
+          dt = dt + sAndFTimes          
         }
+        dt = s2.addText(dt)
+        dt.font=Font.systemFont(8)
         const oDate = new Date(2001,00,01).getTime()
         const nDate = item.startDate.getTime()  
         var diff = ((nDate-oDate)/1000)
         let AllD=item.isAllDay?50000:0
-        diff=Number(diff)+AllD
-        log('diff is '+diff)
-        s.url="calshow:"+diff
+        var diff = ((nDate-oDate)/1000)
+        diff=Number(diff)-tZOffsetSec
+        tx.url=isCalEvent?"calshow:"+diff:"x-apple-reminderkit://"
         s.layoutVertically()
       }
     }
