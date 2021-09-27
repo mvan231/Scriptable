9fb411d568dd1ab08fd356e5c5d96202b5e044c1
mvan231 <50910610+mvan231@users.noreply.github.com>
2021-09-27 12:22:40 -0400

    Optimized

--- Automators.fm Feed/Automators.fm Feed Widget.js
+++ Automators.fm Feed/Automators.fm Feed Widget.js
@@ -7,13 +7,18 @@
 |                                     |
 |          built by: mvan231          |
 |                                     |
+|The 'feed' item below can be any feed|
+|from the automators.fm site          |
+|(i.e. ios/shortcuts)                 |
 |                                     |
-|                                     |
-|                                     |
-|                                     |
+|The 'showThreadPostNum' boolean is   |                                     |used to determine whether the number |
+|of thread posts on the shown thread, |                                    |will be displayed before the title.  |
 |-------------------------------------|
 */
+
 let feed = 'scriptable'
+let showThreadPostNum = true
+
 let r = new Request(`https://talk.automators.fm/c/${feed}.json`)
 let res = await r.loadJSON()
 let userList = res.users
@@ -23,59 +28,57 @@ res=res.topic_list.topics
 
 let slicer = config.widgetFamily=='large'?11:6
 res = res.slice(1,slicer)
-//log(res.length)
-//log(res)
 
 let w = new ListWidget()
 
-let finished = await Promise.all(
+let posts = await Promise.all(
   res.map(async (fin)=> {
-    let stacker = w.addStack()
-    let left = stacker.addStack()
-    stacker.addSpacer(5)
-    let right = stacker.addStack()
-    stacker.url = `https://talk.automators.fm/t/${fin.slug}`
+    let url = `https://talk.automators.fm/t/${fin.slug}`
     let userA = userList.filter(item => {
       if(item.id == fin.posters[0].user_id)return true
       return false
     })
-    //log(userA)
     let avaUrl = `https://talk.automators.fm/${userA[0].avatar_template}`
-    //log(avaUrl)
     avaUrl = avaUrl.replace(/\{size\}/,50)//100)
-    //log(avaUrl)
     r.url = avaUrl
-    let img = await r.loadImage(avaUrl)
-    img = left.addImage(img)
-    img.cornerRadius=15
-    left.layoutVertically()
-    img.resizable=true
-    let t = right.addText(fin.title)
-    t.font = Font.systemFont(12)
-    t.textColor = Color.white()
-    t.shadowColor = Color.black()
-    t.shadowRadius = 3
-    right.layoutVertically()
-    left.size = new Size(0, 30)
-    right.size=new Size(0, 30)
-    stacker.layoutHorizontally()
-    return stacker
+    let avatar_image = await r.loadImage(avaUrl)
+    let title = fin.title
+    let postsCount = fin.posts_count
+    return {url, avatar_image, title, postsCount}
   })
 )
+
+for (post of posts){
+  let stacker = w.addStack()
+  let left = stacker.addStack()
+  stacker.addSpacer(5)
+  let right = stacker.addStack()
+  stacker.url = post.url
+  let img = left.addImage(post.avatar_image)
+  img.cornerRadius=15
+  left.layoutVertically()
+  img.resizable=true
+  let t = right.addText(`${showThreadPostNum?'{'+post.postsCount+'} ':''}${post.title}`)
+  t.font = Font.systemFont(12)
+  t.textColor = Color.white()
+  t.shadowColor = Color.black()
+  t.shadowRadius = 3
+  right.layoutVertically()
+  left.size = new Size(0, 30)
+  right.size=new Size(0, 30)
+  stacker.layoutHorizontally()
+}
 
 if(config.widgetFamily=='large'){
   let im = await getBackground()
   if(im != 'error')w.backgroundImage = im
 }
-
 w.backgroundColor=new Color('204b69')
 
 let grad = new LinearGradient()
 grad.colors = [new Color(Color.lightGray().hex,8/10),new Color(Color.darkGray().hex,10/10)]
 grad.locations = [0,0.75]
-
 w.backgroundGradient = grad
-
 w.setPadding(20,5,20,5)
 
 Script.setWidget(w)
