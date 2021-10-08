e5a97e756c4ff6a662f85e901dabd11103385ace
mvan231 <50910610+mvan231@users.noreply.github.com>
2021-10-08 07:50:42 -0400

    Update exchangeRate.js

--- exchangeRate.js
+++ exchangeRate.js
@@ -4,8 +4,7 @@
 // {"CAD":1.5542,"HKD":9.1412,"ISK":162.8,"PHP":57.083,"DKK":7.4422,"HUF":356.28,"CZK":27.11,"AUD":1.6405,"RON":4.8715,"SEK":10.42,"IDR":17340.01,"INR":86.1985,"BRL":6.5796,"RUB":90.9413,"HRK":7.5765,"JPY":124.95,"THB":36.629,"CHF":1.0773,"SGD":1.5986,"PLN":4.4702,"BGN":1.9558,"TRY":9.3279,"CNY":7.9047,"NOK":10.8623,"NZD":1.7821,"ZAR":19.4315,"USD":1.1795,"MXN":25.0833,"ILS":3.9859,"GBP":0.91167,"KRW":1350.52,"MYR":4.8808}
 
 //updated by mvan231 to allow for free API subscription access
-let base
-if (args.widgetParameter)base=args.widgetParameter
+let base = args.widgetParameter?args.widgetParameter:''
 
 let fm = FileManager.local()
 let path = fm.documentsDirectory()+"/exchangeRateAPIKey.txt"
