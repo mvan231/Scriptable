d384386e4ef57c5f68cfa519f41fe61a2236687d
mvan231 <50910610+mvan231@users.noreply.github.com>
2021-10-10 04:56:13 -0400

    1.1

--- VSCO Recents/VSCO Recents Widget.js
+++ VSCO Recents/VSCO Recents Widget.js
@@ -5,7 +5,7 @@
 
 $$$$$$$$$$$$$$$
 
-VSCO widget
+VSCO Recents Widget
 
 $$$$$$$$$$$$$$$
 
@@ -14,103 +14,147 @@ made by: mvan231
 $$$$$$$$$$$$$$$
 
 version information
-v:1.0
-- Initial Release
-
+v1.1
+- Add menu when tapping the widget so the user can save the image, view the post, or view the profile
 */
 
 //check for an update quick before building the widget
-let needUpdated = await updateCheck(1.0)
+let needUpdated = await updateCheck(1.1)
 
-let url = args.widgetParameter
-if(!url)throw new Error('Please input a profile url in the widget parameter, optionally you can set a secondary input to always show the latest post.\nExample: url|latest')
+let scheme = URLScheme.forRunningScript()
 
-url=url.split('|')
-let latest = url[1]
-url = url[0]
-let r = new Request(url)
-let res = await r.loadString()
-// log(res)
+let url = args.widgetParameter?args.widgetParameter:args.queryParameters
+//url = 'https://vs.co/e119cbf1'
+log(url)
+log(Object.keys(url).length)
+log(url.length)
+if((url.length < 1)||(Object.keys(url).length<1))throw new Error('Please input a profile url in the widget parameter, optionally you can set a secondary input to always show the latest post.\nExample: url|latest')
+let menuOut,menu = url['menu']
+log(menu)
+if(menu){
+  let menuA = new Alert()
+  menuA.title = 'VSCO Recents Widget'
+  menuA.addAction('Save Photo')
+  menuA.addAction('Go To Post')
+  menuA.addAction('Go To Profile')
+  menuA.addCancelAction('Cancel')
+  menuOut = await menuA.presentAlert()
+  log(`menuOut choice is ${menuOut}`)
 
-let reg = /PRELOADED_STATE__ = (.*?)\<\/script\>/
-let regRes = res.match(reg)[1]
-//log(regRes)
-regRes = JSON.parse(regRes)
-let items = regRes.entities.images
-let recentImagesCount = Object.keys(items).length
+  switch (menuOut){
+    case 0:
+      let rr = new Request(url.imgURL)
+      let saveImg = await rr.loadImage()
+      Photos.save(saveImg)
+      menuA=new Alert()
+      menuA.title = 'VSCO Recents Widget'
+      menuA.addAction('OK')
+      menuA.message = 'Photo Saved to Photos App'
+      await menuA.present()
+      App.close()
+      break
+    case 1:
+      Safari.open(url.url)
+      break
+    case 2:
+      Safari.open(`http://vsco.co/${url.user}`)
+      //http://vsco.co/sinyatkin
+      break
+    default:
+      //log('nothing')
+  }
 
-let itemNo = getRandomInt(recentImagesCount)
-log(itemNo)
 
-let itemToGet = Object.keys(items)[latest=='latest'?0:itemNo]
-log(itemToGet)
+}else{
 
-let item = items[itemToGet]
-log(item)
-
-let itemURL = item.permalink
-log(itemURL)
-
-let user = item.permaSubdomain
-log(user)
-
-let imgURL = `https://${item.responsiveUrl}`
-
-r.url = imgURL
-let img = await r.loadImage()
-// QuickLook.present(img, true)
-
-let profileImgURL = regRes.sites.siteByUsername[user].site.profileImage
-log(profileImgURL)
-
-let name = regRes.sites.siteByUsername[user].site.name
-log(name)
-
-log(item.captureDate)
-let capDate = item.captureDate//Math.floor(item.captureDate/1000)
-log(capDate)
-capDate= new Date(capDate)
-
-let dF = new DateFormatter()
-dF.dateFormat = 'MMM d, yyyy'
-log(capDate)
-capDate = dF.string(capDate)
-log(capDate)
-
-let w = new ListWidget()
-
-w.backgroundImage = img
-
-let tStack = w.addStack()
-let dtStack = tStack.addStack()
-let date = dtStack.addText(` ${capDate}${needUpdated?'\n UpdateAvailable!':''} `)
-tStack.addSpacer()
-date.font = Font.lightRoundedSystemFont(config.widgetFamily=='small'?8: 10)
-date.shadowColor = Color.dynamic(Color.black(), Color.white())
-//title.shadowRadius = 2
-dtStack.backgroundColor = Color.dynamic(Color.white(), Color.black())
-dtStack.cornerRadius = 5
-
-w.addSpacer()
-let bStack = w.addStack()
-let userStack = bStack.addStack()
-bStack.addSpacer()
-userStack.layoutVertically()
-let btitle = userStack.addText(` VSCO ${(latest=='latest'||latest==true||latest=='Latest')?'Latest':'Recents'} `)
-btitle.font = Font.lightRoundedSystemFont(config.widgetFamily=='small'?8: 10)
-let userInfo = userStack.addText(` ${name} ${config.widgetFamily=='small'?'\n':'-'} @${user} `)
-userInfo.font = Font.systemFont(config.widgetFamily=='small'?10: 12)
-userInfo.shadowColor = Color.dynamic(Color.white(), Color.black())
-//userInfo.shadowRadius = 15
-userStack.backgroundColor = Color.dynamic(Color.white(), Color.black())
-userStack.cornerRadius = 5
-
-w.url = itemURL
-w.refreshAfterDate = new Date(new Date().getTime()+ 60/*mins*/*60000/*ms/min*/)
-
-Script.setWidget(w)
-Script.complete()
-w.presentLarge()
+  url=url.split('|')
+  let latest = url[1]
+  url = url[0]
+  let r = new Request(url)
+  let res = await r.loadString()
+  // log(res)
+  
+  let reg = /PRELOADED_STATE__ = (.*?)\<\/script\>/
+  let regRes = res.match(reg)[1]
+  //log(regRes)
+  regRes = JSON.parse(regRes)
+  let items = regRes.entities.images
+  let recentImagesCount = Object.keys(items).length
+  
+  let itemNo = getRandomInt(recentImagesCount)
+  //log(`random itemNo is ${itemNo}`)
+  
+  let itemToGet = Object.keys(items)[latest=='latest'?0:itemNo]
+  //log(`item to get is ${itemToGet}`)
+  
+  let item = items[itemToGet]
+  log(item)
+  
+  let itemURL = item.permalink
+  log(itemURL)
+  
+  let user = item.permaSubdomain
+  log(user)
+  
+  let imgURL = `https://${item.responsiveUrl}`
+  
+  r.url = imgURL
+  let img = await r.loadImage()
+  // QuickLook.present(img, true)
+  
+  let profileImgURL = regRes.sites.siteByUsername[user].site.profileImage
+  log(profileImgURL)
+  
+  let name = regRes.sites.siteByUsername[user].site.name
+  log(name)
+  
+  log(item.captureDate)
+  let capDate = item.captureDate//Math.floor(item.captureDate/1000)
+  log(capDate)
+  capDate= new Date(capDate)
+  
+  let dF = new DateFormatter()
+  dF.dateFormat = 'MMM d, yyyy'
+  log(capDate)
+  capDate = dF.string(capDate)
+  log(capDate)
+  
+  let w = new ListWidget()
+  
+  w.backgroundImage = img
+  
+  let tStack = w.addStack()
+  let dtStack = tStack.addStack()
+  let date = dtStack.addText(` ${capDate}${needUpdated?'\n UpdateAvailable!':''} `)
+  tStack.addSpacer()
+  date.font = Font.lightRoundedSystemFont(config.widgetFamily=='small'?8: 10)
+  date.shadowColor = Color.dynamic(Color.black(), Color.white())
+  //title.shadowRadius = 2
+  dtStack.backgroundColor = Color.dynamic(Color.white(), Color.black())
+  dtStack.cornerRadius = 5
+  
+  w.addSpacer()
+  let bStack = w.addStack()
+  let userStack = bStack.addStack()
+  bStack.addSpacer()
+  userStack.layoutVertically()
+  let btitle = userStack.addText(` VSCO ${(latest=='latest'||latest==true||latest=='Latest')?'Latest':'Recents'} `)
+  btitle.font = Font.lightRoundedSystemFont(config.widgetFamily=='small'?8: 10)
+  let userInfo = userStack.addText(` ${name} ${config.widgetFamily=='small'?'\n':'-'} @${user} `)
+  userInfo.font = Font.systemFont(config.widgetFamily=='small'?10: 12)
+  userInfo.shadowColor = Color.dynamic(Color.white(), Color.black())
+  userStack.backgroundColor = Color.dynamic(Color.white(), Color.black())
+  userStack.cornerRadius = 5
+  
+  w.url = `${scheme}?menu=true&url=${itemURL}&imgURL=${imgURL}&user=${user}`
+  //w.url = `${w.url}&openEditor=true`
+  
+  w.refreshAfterDate = new Date(new Date().getTime()+ 60/*mins*/*60000/*ms/min*/)
+  
+  Script.setWidget(w)
+  Script.complete()
+  w.presentLarge()
+}
 
 /*$$$$$$$$$$$$$$$
 
