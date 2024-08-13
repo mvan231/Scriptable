ecf182b59e45b0643558b332e2076bb268dc06d7
mvan231 <50910610+mvan231@users.noreply.github.com>
2024-08-13 02:47:55 -0500

    V5

--- Alexa To Reminders Access.js
+++ Alexa To Reminders Access.js
@@ -12,11 +12,14 @@ Date made: 2023/10/26
 
 Purpose: To sync alexa reminders to a iOS reminders list. previously IFTTT could do this, but Amazon revoked the Alexa IFTTT integration recently.
 
-Setup: Insert the name of the desired reminders list in the "remCal" line. I use "Grocery and Shopping" with my wife, so i have that name entered.
+Setup: 
+  - Insert the Amazon base url your country uses (if different from default) in the "baseURL" variable below
+  - Insert the name of the desired reminders list in the "reminderListName" line. I use "Grocery and Shopping" with my wife, so i have that name entered.
+  - Insert the wording for "Sign In" for the signInKeyvariable below. sometimes this varies based on region
+  - For proper naming preference, please use the withVar and withoutVar for your local language to proeprly set naming of the reminders to be created
 
-When running the first time, the script will check if you are logged in. If not, it will notify and present with login page. After that, the script should run seamlessly. 
 
-When running and items are found to sync, it will show a notification with the items that were added to reminders.
+When running the first time, the script will check if you are logged in. If not, it will notify and present with login page. After that, the script should run seamlessly.
 
 $$$$$$$$$$
 $$$$$$$$$$$$$$$$$$$$$$$
@@ -25,8 +28,8 @@ $$$$$$$$$$$$$$$$$$$$$$$
 $$$$$$$$$$
 
 Version Info:
-v4:modified the wv at beginning to use a variable for the response to aid with crashing
-v3:working version with notification for synced items
+v5 used code from user andereeicheln0z frok github repo issue linked below to inprove performance
+https://github.com/mvan231/Scriptable/issues/25
 
 $$$$$$$$$$
 $$$$$$$$$$$$$$$$$$$$$$$
@@ -34,9 +37,18 @@ $$$$$$$$$$$$$$$$$$$$$$$
 
 //set baseURL based on your home country url
 const baseURL = 'https://www.amazon.com'
+
+//include the reminder list name exactly as it is in Reminders app
 const reminderListName = 'Grocery and Shopping'
+
 //signInKey should be specific for your language. English uses "Sign In". German uses "Anmelden"
 const signInKey = "Sign In"
+
+//withVar below needs to be set to your language's version of the word 'with'
+const withVar = "with"
+
+//withoutVar below needs to be set to your language's version of the word 'without'
+const withoutVar = "without"
 
 main();
 Script.complete();
@@ -86,11 +98,19 @@ async function synchronizeReminders() {
     const deleteUrl = `${baseURL}/alexashoppinglists/api/deletelistitem`;
     const json = await new Request(url).loadJSON();
     const listItems = json[Object.keys(json)[0]].listItems;
-    const existingReminders = await Reminder.all([reminderCalendar]);
+    //const existingReminders = await Reminder.all([reminderCalendar]);
 
     for (const item of listItems) {
-      const reminderTitle = item.value.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-      const reminderExists = existingReminders.some(reminder => reminder.title === reminderTitle);
+      const reminderTitle = item.value.split(' ').map(word => {
+        if (word.toLowerCase() === withVariable || word.toLowerCase() === withoutVar) {
+          return word.toLowerCase();
+        } else {
+          return word.charAt(0).toUpperCase() + word.slice(1);
+        }
+      }).join(' ');
+      const allReminders = await Reminder.all([reminderCalendar]);
+      const incompleteReminders = allReminders.filter(reminder => !reminder.isCompleted);
+      const reminderExists = incompleteReminders.some(reminder => reminder.title === reminderTitle);
 
       if (!reminderExists) {
         const reminder = new Reminder();
